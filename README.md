048f63f63373b31d58001f1d5db0c885e2c41202
Emiliano Dalla Verde Marcozzi <6564766d@gmail.com>
2020-03-13 16:06:01 -0300

    Is alive!

--- README.md
+++ README.md
@@ -3,12 +3,12 @@ Use python 3.9-dev or superior
 # Notes
 Management suggests the use of:
 
-- Httpie (sudo apt install httpie)
-
 - Pyenv (for virtualenv creation and python activation)
 
 
 # Test it
 
-http localhost:8000/fast/58611129-2dbc-4a81-a72f-77ddfc1b1b49
+curl -o /dev/null -w  "%{time_starttransfer}\n" http://localhost:8000/slow/
+
+curl -o /dev/null -w  "%{time_starttransfer}\n" http://localhost:8000/fast/
 
--- src/main.py
+++ src/main.py
@@ -3,6 +3,12 @@ import httpx
 
 app = FastAPI()
 anime_url = "https://ghibliapi.herokuapp.com/films"
+anime_ids = [
+    "2baf70d1-42bb-4437-b551-e5fed5a87abe",
+    "12cfb892-aac0-4c5b-94af-521852e46d6a",
+    "58611129-2dbc-4a81-a72f-77ddfc1b1b49",
+    "ea660b10-85c4-4ae3-8a5f-41cea3648e3e",
+]
 
 
 @app.get("/")
@@ -10,9 +16,21 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/fast/{anime_id}")
-def get_info_fast(anime_id: str):
-    url = f"{anime_url}/{anime_id}"
-    response = httpx.get(url)
-    return response.json()
+@app.get("/slow/")
+def get_info():
+    data = []
+    for url in [f"{anime_url}/{anime_id}" for anime_id in anime_ids]:
+        response = httpx.get(url)
+        data.append(response.json())
+    return data
+
+
+@app.get("/fast/")
+async def get_info_fast():
+    data = []
+    async with httpx.AsyncClient() as client:
+        for url in [f"{anime_url}/{anime_id}" for anime_id in anime_ids]:
+            response = await client.get(url)
+            data.append(response.json())
+    return data
 
